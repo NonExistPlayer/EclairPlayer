58d6381d759b9a5b57001e144b7943da679cf106
NonExistPlayer <ilya08game@gmail.com>
2024-12-28 14:42:29 +0300

    Added `READ_MEDIA_AUDIO` permission.

--- Eclair.Android/MainActivity.cs
+++ Eclair.Android/MainActivity.cs
@@ -1,5 +1,9 @@
-﻿using Android.App;
+﻿using Android;
+using Android.App;
 using Android.Content.PM;
+using Android.OS;
+using AndroidX.Core.App;
+using AndroidX.Core.Content;
 using Avalonia;
 using Avalonia.Android;
 using Avalonia.ReactiveUI;
@@ -14,6 +18,12 @@ namespace Eclair.Android;
     ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
 public class MainActivity : AvaloniaMainActivity<App>
 {
+#pragma warning disable CA1416
+    private readonly static string read_permission =
+        Build.VERSION.SdkInt >= BuildVersionCodes.Tiramisu /* (Android 13) */ ?
+        Manifest.Permission.ReadMediaAudio : Manifest.Permission.ReadExternalStorage;
+#pragma warning restore CA1416
+
     protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
     {
         Main.PManager = new PlatformManager();
@@ -21,6 +31,30 @@ public class MainActivity : AvaloniaMainActivity<App>
         return base.CustomizeAppBuilder(builder)
             .WithInterFont()
             .UseReactiveUI();
+    }
+
+    protected override void OnCreate(Bundle? savedInstanceState)
+    {
+        if (ContextCompat.CheckSelfPermission(this, read_permission) != (int)Permission.Granted)
+        {
+            Main.Logger.Log($"Requesting {read_permission}...");
+            ActivityCompat.RequestPermissions(this, [read_permission], 1);
+        }
+
+        base.OnCreate(savedInstanceState);
+    }
+
+    public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+    {
+        base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1)
+        {
+            if (grantResults.Length > 0 && grantResults[0] == Permission.Granted)
+                Main.Logger.Log($"{read_permission} granted!");
+            else
+                Main.Logger.Log($"{read_permission} not granted.");
+        }
     }
 
     protected override void Dispose(bool disposing)
